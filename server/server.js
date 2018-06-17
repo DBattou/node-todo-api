f15d1ad5333ca2afa889e8b613c68e5246dfa10f
Baptiste Doucerain <doucerain.b@gmail.com>
2018-06-17 17:12:37 +0200

    ADdd post todos route, refacto mongoose

---
+++ server/server.js
@@ -0,0 +1,31 @@
+const { mongoose } = require('./db/mongoose');
+var bodyParser = require('body-parser');
+var express = require('express');
+
+var { Todo } = require('./models/todo');
+var { User } = require('./models/user');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.get('/hello', (req, res) => {
+  res.send('Hello world');
+  console.log('HelloWorld');
+});
+
+app.post('/todos', (req, res) => {
+  var todo = new Todo({
+    text: req.body.text
+  })
+
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
+app.listen(3000, () => {
+  console.log('App started on port 3000');
+});
