fdd09dfee3be74df7a87c9dd0b295198455d3388
Baptiste Doucerain <doucerain.b@gmail.com>
2018-07-26 13:52:38 +0200

    Add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -69,6 +69,41 @@ UserSchema.statics.findByToken = function(token) {
   });
 }
 
+UserSchema.statics.findByCredentials = function(email, password) {
+  var User = this;
+
+  return User.findOne({ email }).then((user) => {
+    if (!user) {
+      return Promise.reject();
+    }
+
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res === true) {
+          resolve(user);
+        } else {
+          reject();
+        }
+      })
+    })
+  })
+}
+
+// UserSchema.pre('save', function (next) {
+//   var user = this;
+//
+//   if (user.isModified('password')) {
+//     bcrypt.genSalt(10, (err, salt) => {
+//       bcrypt.hash(user.password, salt, (err, hash) => {
+//         user.password = hash;
+//         next();
+//       });
+//     });
+//   } else {
+//     next();
+//   }
+// });
+
 UserSchema.pre('save', function(next) {
   var user = this;
   if (user.isModified('password')) {
@@ -76,10 +111,10 @@ UserSchema.pre('save', function(next) {
       bcrypt.hash(user.password, salt, (err, hash) => {
         user.password = hash;
         next();
-      })
-    })
+      });
+    });
   } else {
-
+    next();
   }
 });
 // User
