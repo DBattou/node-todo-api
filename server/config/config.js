5a7ff6d29be56a4a158e180d83675c5a67b4278a
Baptiste Doucerain <doucerain.b@gmail.com>
2018-08-02 13:45:35 +0200

    Delete trailing logs

--- server/config/config.js
+++ server/config/config.js
@@ -1,12 +1,9 @@
 var env = process.env.NODE_ENV || 'development';
 
 if (env === 'development' || env === 'test') {
-  console.log('CPOUCOUCOUCOUCOUCOUOCUC');
   let config = require('./config.json');
   let envConfig = config[env];
   Object.keys(envConfig).forEach((key) => {
-    console.log(key + ' : ');
-    console.log(envConfig[key]);
     process.env[key] = envConfig[key];
   });
 };
