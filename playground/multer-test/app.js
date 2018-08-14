f0a39c9d8b29e8013432138b8f223f03b2dfa276
Baptiste Doucerain <baptiste.doucerain.ext@parrot.com>
2018-08-14 18:04:58 +0200

    Some experimentations have made

--- playground/multer-test/app.js
+++ playground/multer-test/app.js
@@ -1,16 +1,23 @@
 const express = require('express');
-const multer = require('multer');
-const ejs = require('ejs');
-const path = require('path');
+// const multer = require('multer');
 
 const port = 3000;
 
 let app = express();
 
-app.set('view engine', 'ejs');
-
 app.use(express.static('./public'));
 
-app.get('/', (req, res) => res.render('index'));
+app.get('/', (req, res) => {
+    res.send('YOOOOLOOOO');
+})
+
+// app.post('/upload/:file', (req, res)=>{
+//     let path = req
+// })
+
+app.get('/download/:file', (req, res) => {
+    let file = req.params.file;
+    return res.download('./public/' + file);
+})
 
 app.listen(port, () => console.log(`app listen on port ${port}`));
